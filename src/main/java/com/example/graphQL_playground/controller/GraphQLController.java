5b48556806949f6c7e92e72012244f0d57b3c535
pranay290491 <pranay.dmnsns@gmail.com>
2025-02-15 09:27:29 +0530

    passed argument to g-ql server

--- src/main/java/com/example/graphQL_playground/controller/GraphQLController.java
+++ src/main/java/com/example/graphQL_playground/controller/GraphQLController.java
@@ -1,5 +1,6 @@
 package com.example.graphQL_playground.controller;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -9,5 +10,10 @@ public class GraphQLController {
     @QueryMapping
     public String sayHello(){
         return "Hello World!";
+    }
+
+    @QueryMapping("passArgs")
+    public String acceptingArgs(@Argument int age){
+        return String.valueOf(age)+" returned Int in String format";
     }
 }
