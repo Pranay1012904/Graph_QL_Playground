cd6fe06ae4d27d1e320b49ac00352e817b851d6a
pranay290491 <pranay.dmnsns@gmail.com>
2025-02-15 17:39:08 +0530

    added age related functionality

--- src/main/java/com/example/graphQL_playground/controller/lec02/CustomerController.java
+++ src/main/java/com/example/graphQL_playground/controller/lec02/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.graphQL_playground.controller.lec02;
 import com.example.graphQL_playground.controller.lec02.service.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,5 +19,19 @@ public class CustomerController {
     @QueryMapping("customers")
     public List<CustomerEntity> getAllCustomers(){
        return customerService.getCustomers();
+    }
+    @QueryMapping
+    public List<CustomerEntity> customerById(@Argument Integer id){
+        return customerService.getCustomerById(id);
+    }
+
+    @QueryMapping
+    public List<CustomerEntity> CustomersByName(@Argument String name){
+        return customerService.getCustomerByName(name);
+    }
+
+    @QueryMapping
+    public  List<CustomerEntity> findCustomerByAgeRange(@Argument Integer minAge,@Argument Integer maxAge){
+        return customerService.findCustomerByAgeRange(minAge, maxAge);
     }
 }
--- src/main/java/com/example/graphQL_playground/controller/lec02/service/CustomerService.java
+++ src/main/java/com/example/graphQL_playground/controller/lec02/service/CustomerService.java
@@ -9,11 +9,13 @@ import java.util.List;
 public class CustomerService {
 
     public List<CustomerEntity> getCustomers(){
-        CustomerEntity customer1=new CustomerEntity(1,"pranay",34,"Ranchi");
+        CustomerEntity customer1=new CustomerEntity(1,"pran",34,"Ranchi");
         CustomerEntity customer2=new CustomerEntity(2,"divya",44,"Pune");
         CustomerEntity customer3=new CustomerEntity(3,"pranay",55,"Mumbai");
-        CustomerEntity customer4=new CustomerEntity(4,"pranay",23,"Koderma");
-        CustomerEntity customer5=new CustomerEntity(5,"pranay",21,"Thane");
+        CustomerEntity customer4=new CustomerEntity(4,"pranay_Ranjan",23,"Koderma");
+        CustomerEntity customer5=new CustomerEntity(5,"ronny_pranay",21,"Thane");
+        CustomerEntity customer6=new CustomerEntity(4,"sammy",33,"Koderma");
+        CustomerEntity customer7=new CustomerEntity(5,"rohan",18,"Thane");
         List<CustomerEntity> customerEntityList= Arrays.asList(customer1,customer2,customer3,customer4,customer5);
         return customerEntityList;
     }
@@ -26,5 +28,9 @@ public class CustomerService {
     public  List<CustomerEntity> getCustomerByName(String name){
         List<CustomerEntity> fetchedCustomers= getCustomers().stream().filter(c-> c.getName().contains(name)).toList();
         return fetchedCustomers;
+    }
+
+    public List<CustomerEntity> findCustomerByAgeRange(Integer minAge, Integer maxAge){
+     return   getCustomers().stream().filter(c-> (c.getAge()>=minAge && c.getAge()<=maxAge)).toList();
     }
 }
